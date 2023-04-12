8a8458cb0096ac1227708bd489a7ea5bd5025433
priyanka8420 <vasantha8420@gmail.com>
2023-04-12 20:51:43 +0530

    javascript

--- NodeJs/Topic1/pg1_index.js
+++
@@ -1,9 +0,0 @@
-// Default imports //
-// const add = require("./pg1_cal");
-// console.log(add(6, 3));
-
-//  named module importing//
-const { add, sub } = require("./pg1_cal");
-console.log(add(6, 3));
-console.log(sub(6, 3));
-
--- NodeJs/Topic1/pg5_core_modules.py
+++
@@ -1,4 +0,0 @@
-const path = require("path");
-
-const filePath = path.join("users", "pavan", "notes.txt");
-console.log(filePath);
